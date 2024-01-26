2fe297449a8c1f323dec57c67f8c23b89a86e762
Yasiel <yasielglez90210@gmail.com>
2024-01-25 21:17:29 -0500

    improving the video preview process

--- components/vimeo/VideoItem.tsx
+++ components/vimeo/VideoItem.tsx
@@ -4,63 +4,53 @@ import { Link } from '@/navigation'
 import type { Video } from '@/types/vimeo'
 import Image from 'next/image'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
-import { useState } from 'react'
-import dynamic from 'next/dynamic'
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false })
+import { LegacyRef, useEffect, useRef, useState } from 'react'
+import Player from '@vimeo/player'
 
 export default function VideoItem({ video }: { video: Video }) {
   const id = video.uri.split('/').pop()
   const [isHovered, setIsHovered] = useState(false)
-  const [play, setPlay] = useState(false)
+  const imageRef = useRef<HTMLImageElement | null>(null)
+  const playerRef = useRef<HTMLElement | null>(null)
 
-  const onPlay = () => {
-    setPlay((prev) => !prev)
-  }
-
-  const onMouseEnter = () => {
+  const onMouse = () => {
     setIsHovered((prev) => !prev)
   }
 
-  const onMouseLeave = () => {
-    if (play) setPlay((prev) => !prev)
-    setIsHovered((prev) => !prev)
-  }
+  useEffect(() => {
+    if (isHovered && playerRef.current) {
+      const player = new Player(playerRef.current)
+
+      if (imageRef.current) imageRef.current.classList.add('rounded-none')
+
+      player.on('play', () => {
+        if (playerRef.current) playerRef.current.classList.remove('opacity-0')
+      })
+    } else {
+      if (imageRef.current) imageRef.current.classList.remove('rounded-none')
+    }
+  }, [isHovered])
 
   return (
-    <Link
-      href={`/video/${id}`}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
+    <Link href={`/video/${id}`} onMouseEnter={onMouse} onMouseLeave={onMouse}>
       <div className="flex flex-col">
         <AspectRatio ratio={16 / 9} className="mb-2">
-          {!play && (
-            <Image
-              className="rounded-lg object-cover"
-              fill
-              priority
-              sizes="cover"
-              src={video.pictures.base_link}
-              alt={video.name}
-            />
-          )}
+          <Image
+            ref={imageRef}
+            className="rounded-lg object-cover transition-all duration-300 ease-in-out"
+            fill
+            priority
+            sizes="cover"
+            src={video.pictures.base_link}
+            alt={video.name}
+          />
 
           {isHovered && (
-            <ReactPlayer
-              url={video.link}
-              width="100%"
-              height="100%"
-              controls={false}
-              muted
-              playing
-              loop
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: 0,
-              }}
-              onPlay={onPlay}
-            />
+            <iframe
+              className="opacity-0"
+              ref={playerRef as LegacyRef<HTMLIFrameElement>}
+              src={`${video.player_embed_url}?portrait=0&transparent=0&autoplay=1&muted=1&background=1&autopause=1&loop=1&quality=auto&responsive=1`}
+            ></iframe>
           )}
         </AspectRatio>
         <div className="pr-4">
